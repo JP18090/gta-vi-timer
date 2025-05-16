67c5a0cb51fb0e14b0f36391066eedacfcaef614
José Pedro <166343703+JP18090@users.noreply.github.com>
2025-05-15 23:00:58 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = "Contagem GTA VI";
+const urlsToCache = ["index.html", "manifest.json", "icons/LogoGTA6.png", "icons/LogoGTAVI.png"];
+
+// Instala e adiciona ao cache
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Responde com cache se estiver offline
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
